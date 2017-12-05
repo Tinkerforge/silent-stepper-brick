7e27a9d52bc02a9eaf447ce83447c72d53f461ec
Olaf Lüke <olaf@tinkerforge.com>
2017-12-05 16:04:16 +0100

    Remove initial USB enumeration (now done in bricklib)

--- software/src/silent-stepper.c
+++ software/src/silent-stepper.c
@@ -105,7 +105,6 @@ const uint32_t stepper_timer_velocity[]  = {BOARD_MCK/2   / MAX_TIMER_VALUE,
                                             32768         / MAX_TIMER_VALUE};
 
 extern ComInfo com_info;
-extern bool usb_first_connection;
 
 void stepper_position_reached_signal(void) {
 	PositionReachedSignal prs;
@@ -275,18 +274,6 @@ void tick_task(const uint8_t tick_type) {
 
 		stepper_all_data_period_counter++;
 	} else if(tick_type == TICK_TASK_TYPE_MESSAGE) {
-		if(usb_first_connection && !usbd_hal_is_disabled(IN_EP)) {
-			message_counter++;
-			if(message_counter >= 100) {
-				message_counter = 0;
-				if(brick_init_enumeration(COM_USB)) {
-					com_info.current = COM_USB;
-					message_counter = 0;
-					usb_first_connection = false;
-				}
-			}
-		}
-
 		stepper_tick_counter++;
 
 		if(stepper_position_reached) {
