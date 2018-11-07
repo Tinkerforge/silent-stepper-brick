73d971c757b7724b93baaa59f3a730cd6e2015fb
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:27:36 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,6 +1,6 @@
 use rand::{thread_rng, Rng};
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, silent_stepper_brick::*};
+use tinkerforge::{ip_connection::IpConnection, silent_stepper_brick::*};
 
 const HOST: &str = "127.0.0.1";
 const PORT: u16 = 4223;
@@ -8,19 +8,19 @@ const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your Silent Stepper B
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ipcon = IpConnection::new(); // Create IP connection
-    let silent_stepper_brick = SilentStepperBrick::new(UID, &ipcon); // Create device object
+    let ss = SilentStepperBrick::new(UID, &ipcon); // Create device object
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
+                                          // Don't use device before ipcon is connected
 
-    //Create listener for position reached events.
-    let position_reached_listener = silent_stepper_brick.get_position_reached_receiver();
-    // Spawn thread to handle received events. This thread ends when the silent_stepper_brick
+    //Create receiver for position reached events.
+    let position_reached_receiver = ss.get_position_reached_receiver();
+    // Spawn thread to handle received events. This thread ends when the ss
     // is dropped, so there is no need for manual cleanup.
-    let silent_stepper_brick_copy = silent_stepper_brick.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
+    let ss_copy = ss.clone(); //Device objects don't implement Sync, so they can't be shared between threads (by reference). So clone the device and move the copy.
     thread::spawn(move || {
         let mut rng = thread_rng();
-        for _event in position_reached_listener {
+        for position_reached in position_reached_receiver {
             let steps = if rng.gen() {
                 let steps = rng.gen_range(1000, 5001); // steps (forward)
                 println!("Driving forward: {} steps", steps);
@@ -37,20 +37,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             println!("Configuration (vel, acc, dec): ({}, {}, {})", vel, acc, dec);
 
-            silent_stepper_brick_copy.set_speed_ramping(acc, dec);
-            silent_stepper_brick_copy.set_max_velocity(vel);
-            silent_stepper_brick_copy.set_steps(steps);
+            ss_copy.set_speed_ramping(acc, dec);
+            ss_copy.set_max_velocity(vel);
+            ss_copy.set_steps(steps);
         }
     });
 
-    silent_stepper_brick.set_step_configuration(SILENT_STEPPER_BRICK_STEP_RESOLUTION_8, true); // 1/8 steps (interpolated)
-    silent_stepper_brick.enable(); // Enable motor power
-    silent_stepper_brick.set_steps(1); // Drive one step forward to get things going
+    ss.set_step_configuration(SILENT_STEPPER_BRICK_STEP_RESOLUTION_8, true); // 1/8 steps (interpolated)
+    ss.enable(); // Enable motor power
+    ss.set_steps(1); // Drive one step forward to get things going
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    silent_stepper_brick.disable();
+    ss.disable();
     ipcon.disconnect();
     Ok(())
 }
--- software/examples/rust/example_configuration.rs
+++ software/examples/rust/example_configuration.rs
@@ -1,33 +1,33 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, silent_stepper_brick::*};
+use tinkerforge::{ip_connection::IpConnection, silent_stepper_brick::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your Silent Stepper Brick
+const UID: &str = "XXYYZZ"; // Change XXYYZZ to the UID of your Silent Stepper Brick.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let silent_stepper_brick = SilentStepperBrick::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ss = SilentStepperBrick::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    silent_stepper_brick.set_motor_current(800); // 800mA
-    silent_stepper_brick.set_step_configuration(SILENT_STEPPER_BRICK_STEP_RESOLUTION_8, true); // 1/8 steps (interpolated)
-    silent_stepper_brick.set_max_velocity(2000); // Velocity 2000 steps/s
+    ss.set_motor_current(800); // 800mA
+    ss.set_step_configuration(SILENT_STEPPER_BRICK_STEP_RESOLUTION_8, true); // 1/8 steps (interpolated)
+    ss.set_max_velocity(2000); // Velocity 2000 steps/s
 
     // Slow acceleration (500 steps/s^2),
     // Fast deacceleration (5000 steps/s^2)
-    silent_stepper_brick.set_speed_ramping(500, 5000);
+    ss.set_speed_ramping(500, 5000);
 
-    silent_stepper_brick.enable(); // Enable motor power
-    silent_stepper_brick.set_steps(60000); // Drive 60000 steps forward
+    ss.enable(); // Enable motor power
+    ss.set_steps(60000); // Drive 60000 steps forward
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    silent_stepper_brick.disable();
+    ss.disable();
     ipcon.disconnect();
     Ok(())
 }
