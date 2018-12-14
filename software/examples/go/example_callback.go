1b83a4abc514cbb33042c65ca13a2bc1b4ccde7b
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:15:48 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"tinkerforge/ipconnection"
+    "tinkerforge/silent_stepper_brick"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XXYYZZ" // Change XXYYZZ to the UID of your Silent Stepper Brick.
+
+func main() {
+	ipcon := ipconnection.New()
+    defer ipcon.Close()
+	ss, _ := silent_stepper_brick.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+    defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+    
+    rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+    
+	ss.RegisterPositionReachedCallback(func(position int32) {
+		steps := int32(0)
+		if rng.Int31() % 2 == 0 {
+            steps = rng.Int31n(4001) + 1000
+            fmt.Printf("Driving forward: %d steps\n", steps)
+        } else {
+            steps = -(rng.Int31n(4001) + 1000)
+            fmt.Printf("Driving backward: %d steps\n", steps)
+        }
+        
+        vel := uint16(rng.Int31n(1800) + 200)
+        acc := uint16(rng.Int31n(900) + 100)
+        dec := uint16(rng.Int31n(900) + 100)
+        
+        fmt.Printf("Configuration: (vel, acc, dec): (%d, %d, %d)\n", vel, acc, dec)
+        ss.SetSpeedRamping(acc, dec)
+        ss.SetMaxVelocity(vel)
+        ss.SetSteps(steps)
+	})
+
+	ss.SetStepConfiguration(silent_stepper_brick.StepResolution8,
+		true) // 1/8 steps (interpolated)
+	ss.Enable()    // Enable motor power
+	ss.SetSteps(1) // Drive one step forward to get things going
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+	ss.Disable()
+	ipcon.Disconnect()
+}
---
+++ software/examples/go/example_configuration.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/silent_stepper_brick"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XXYYZZ" // Change XXYYZZ to the UID of your Silent Stepper Brick.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	ss, _ := silent_stepper_brick.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	ss.SetMotorCurrent(800) // 800mA
+	ss.SetStepConfiguration(silent_stepper_brick.StepResolution8,
+		true) // 1/8 steps (interpolated)
+	ss.SetMaxVelocity(2000) // Velocity 2000 steps/s
+
+	// Slow acceleration (500 steps/s^2),
+	// Fast deacceleration (5000 steps/s^2)
+	ss.SetSpeedRamping(500, 5000)
+
+	ss.Enable()        // Enable motor power
+	ss.SetSteps(60000) // Drive 60000 steps forward
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+	ss.Disable()
+}
