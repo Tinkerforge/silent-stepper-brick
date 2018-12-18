a70843fae4a47065f1f6edc553cf8412d18c2c6a
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:25:06 +0100

    go: Change import path again.

--- software/examples/go/example_callback.go
+++ software/examples/go/example_callback.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-	"github.com/tinkerforge/go-api-bindings/ipconnection"
-    "github.com/tinkerforge/go-api-bindings/silent_stepper_brick"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
+    "github.com/Tinkerforge/go-api-bindings/silent_stepper_brick"
 )
 
 const ADDR string = "localhost:4223"
